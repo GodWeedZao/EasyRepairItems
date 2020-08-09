3a683e1242a05f54e2d8df5cb3821925aee87e65
GodWeedZao <64776465+GodWeedZao@users.noreply.github.com>
2020-08-10 00:56:16 +0430

    Update RepairCommand.php

--- src/GodWeedZao/EasyRepairItems/RepairCommands/RepairCommand.php
+++ src/GodWeedZao/EasyRepairItems/RepairCommands/RepairCommand.php
@@ -41,33 +41,16 @@ class RepairCommand
                 return true;
             }
             if ($cmd->getName() === "repair") {
-                if (!isset($args[0])) {
-                    $player->sendMessage("§l§cUsage: §4/repair item");
-                    if (Main::Config("MONEY") === true) {
-                        $player->sendMessage("§l§c>§aPaymentMode§2: §eMoney");
-                        $EconomyAPI = Server::getInstance()->getPluginManager()->getPlugin("EconomyAPI");
-                        $money = $EconomyAPI->myMoney($player);
-                        $player->sendMessage("§l§c>§aYour Money§2: §6{$money}");
-                    }
-                    if (Main::Config("XP") === true) {
-                        $player->sendMessage("§l§c>§aPaymentMode§2: §eXp");
-                        $xp = $player->getXpLevel();
-                        $player->sendMessage("§l§c>§aYour Xp§2: §6{$xp}");
-                    }
-                }
-                if ((isset($args[0])) && ($args[0] === "item")) {
-                    $slot = $player->getInventory()->getHeldItemIndex();
-                    $Item = $player->getInventory()->getItem($slot);
-                    if ($this->plugin->isItem($Item) || $this->plugin->isArmor($Item)) {
-                        $repairItem = new RepairItems ($this->plugin->getInstance());
-                        $repairItem->RepairItem($player);
-                    } else {
-                        $player->sendMessage("§l§4Item Held Item is not Tool/Armor, Repair Failed.");
-                    }
+                $slot = $player->getInventory()->getHeldItemIndex();
+                $Item = $player->getInventory()->getItem($slot);
+                if (($this->plugin->isItem($Item)) || ($this->plugin->isArmor($Item))) {
+                    $repairItem = new RepairItems ($this->plugin->getInstance());
+                    $repairItem->RepairItem($player);
+                } else {
+                    $player->sendMessage("§l§4Item Held Item is not Tool/Armor, Repair Failed.");
                 }
             }
         }
         return true;
     }
 }
-    
